8d10787514c821dfeb8d2dee631d365435bb4965
Лысенко Андрей <rostangeles1993@gmail.com>
2025-05-02 15:41:06 +0300

    Dev app me-degree

--- app/element-list.php
+++ app/element-list.php
@@ -1,7 +1,5 @@
 <?php
 
-use Soap\Url;
-
 class User
 {
 	private string $name;
@@ -24,7 +22,7 @@ class User
 		return $this->next;
 	}
 
-	public function setNext(?User $next): void
+	public function setNext(User $next): void
 	{
 		$this->next = $next;
 	}
@@ -123,34 +121,18 @@ class UsersList
 		} else {
 			$this->tail = $prevUser;
 		}
+
 		return true;
 	}
 }
 
 $list = new UsersList();
-$aleksey = $list->add('Aleksey');
-$andrey = $list->add('Andrey');
-$ilya = $list->add('Ilya');
+$list->add('Aleksey');
+$list->add('Andrey');
+$list->add('Ilya');
 
-
-$searchedElement = $list->find('Aleksey');
-
-echo $searchedElement
-	? 'User is founded'
-	: 'User not found';
-echo "<br>";
-
-echo "Remove Aleksey: " . ($list->remove('Aleksey')
-	? "Success"
-	: "Failed") . "<br>";
-
-echo "Remove non-existent: " . ($list->remove('Non-exists')
-	? "Success"
-	: "Failed") . "<br>";
-
-echo "Лист после удаления: <br>";
-$current = $list->getHead();
-while ($current !== null) {
-	echo $current->getName() . "<br>";
-	$current = $current->getNext();
+if ($list->remove('Ilya') === true) {
+	echo "Пользователь удален";
+} else {
+	echo "Не удалось удалить пользователя";
 }
